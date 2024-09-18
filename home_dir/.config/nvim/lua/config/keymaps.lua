9cc2c89414ac95d527c1bea265db92b43e2bb28c
Bobby Valenzuela <bobby.valenzuela@protonmail.com>
2024-09-18 15:03:40 -0700

    Updated nvmi conf files

--- home_dir/.config/nvim/lua/config/keymaps.lua
+++ home_dir/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- -- ~/.config/nvim/lua/keymaps.lua or wherever your keymaps are configured
+-- Map Ctrl+b to enter visual-block mode
+vim.keymap.set('n', '<C-b>', '<C-v>', { noremap = true, silent = true })
+
--- home_dir/nvim/lua/config/keymaps.lua
+++
@@ -1,7 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
--- -- ~/.config/nvim/lua/keymaps.lua or wherever your keymaps are configured
--- Map Ctrl+b to enter visual-block mode
-vim.keymap.set('n', '<C-b>', '<C-v>', { noremap = true, silent = true })
-
