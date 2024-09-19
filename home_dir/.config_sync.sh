5681f51ec2aa33efe2d9947d8b482d06712f00a3
Bobby Valenzuela <bobby.valenzuela@protonmail.com>
2024-09-18 17:47:29 -0700

    Synced conf file

---
+++ home_dir/.config_sync.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+
+echo "Running..."
+
+DEST=~/my_repos/Dev-Environment/home_dir/
+cp -v ~/.bash_aliases  $DEST
+cp -v ~/.bash_git  $DEST
+cp -v ~/.bash_pbx  $DEST
+cp -v ~/.bash_utils  $DEST
+cp -v ~/.bashrc  $DEST
+cp -v -r ~/.config/  $DEST
+cp -v ~/.p10k.zsh  $DEST
+cp -v ~/.tmux.conf  $DEST
+cp -v ~/.vimrc  $DEST
+cp -v ~/.zshrc   $DEST
+cp -v ~/.config_sync.sh  $DEST
+echo "Done!"
