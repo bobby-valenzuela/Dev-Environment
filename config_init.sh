56f07225b9228fd02643e65d2a065586614b52ca
Bobby Valenzuela <70870649+bobby-valenzuela@users.noreply.github.com>
2025-04-03 11:01:47 -0700

    Update config_init.sh

--- config_init.sh
+++ config_init.sh
@@ -9,6 +9,11 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
+# Maintain user's home if calling with sudo
+if [ "$SUDO_USER" ]; then
+    HOME=$(getent passwd "$SUDO_USER" | cut -d: -f6)
+fi
+
 # Execute from the dir the script is in
 cd "$(dirname "$0")" || exit
 
@@ -20,7 +25,7 @@ cp -v ./config/.bash_utils  $HOME
 cp -v ./config/.bashrc  $HOME
 cp -v ./config/.p10k.zsh  $HOME
 cp -v ./config/.tmux.conf  $HOME
-cp -v ./config/nvim  $HOME
+cp -v ./config/.config/nvim  $HOME
 cp -v ./config/.vimrc  $HOME
 cp -v ./config/.zshrc   $HOME
 cp -v ./config/.tmux_init.sh  $HOME
@@ -29,7 +34,7 @@ cp -v -r ./config/.config/nvim  $HOME
 cp -v ./config/usr_local_bin/* /usr/local/bin/
 
 
-if [[ "$1" == "install"]];then
+if [[ "$1" == "install" ]];then
     printf "[+] Installing packages...\n\n"
     sudo apt-get update && apt install python3 wget zsh lua5.4 curl tar ripgrep fzf make gcc unzip git git-all xclip build-essential p7zip-full jq locatesshpass xsel cmake libstdc++6 -y
 
@@ -40,7 +45,7 @@ if [[ "$1" == "install"]];then
     curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
     sudo rm -rf /opt/nvim
     sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-    echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> $HOME/.zshrc
+    echo 'export PATH="/opt/nvim-linux-x86_64/bin:$PATH"' >> $HOME/.zshrc
 
     printf "[+] Installing powerlevel10k...\n\n"
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
