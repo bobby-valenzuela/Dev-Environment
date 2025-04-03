45e0737aafca266877dd630dff84e04f6e338647
Bobby Valenzuela <70870649+bobby-valenzuela@users.noreply.github.com>
2025-04-03 11:07:38 -0700

    Update config_init.sh

--- config_init.sh
+++ config_init.sh
@@ -18,18 +18,18 @@ fi
 cd "$(dirname "$0")" || exit
 
 # Sync local config files with cloned repo so i can push up my changes
-cp -v ./config/.bash_aliases  $HOME
-cp -v ./config/.bash_git  $HOME
-cp -v ./config/.bash_pbx  $HOME
-cp -v ./config/.bash_utils  $HOME
-cp -v ./config/.bashrc  $HOME
-cp -v ./config/.p10k.zsh  $HOME
-cp -v ./config/.tmux.conf  $HOME
-cp -v ./config/.config/nvim  $HOME
-cp -v ./config/.vimrc  $HOME
-cp -v ./config/.zshrc   $HOME
-cp -v ./config/.tmux_init.sh  $HOME
-cp -v -r ./config/.config/nvim  $HOME
+cp -v ./config/.bash_aliases  $HOME/
+cp -v ./config/.bash_git  $HOME/
+cp -v ./config/.bash_pbx  $HOME/
+cp -v ./config/.bash_utils  $HOME/
+cp -v ./config/.bashrc  $HOME/
+cp -v ./config/.p10k.zsh  $HOME/
+cp -v ./config/.tmux.conf  $HOME/
+cp -v ./config/.config/nvim  $HOME/
+cp -v ./config/.vimrc  $HOME/
+cp -v ./config/.zshrc   $HOME/
+cp -v ./config/.tmux_init.sh  $HOME/
+cp -v -r ./config/.config/nvim  $HOME/
 
 cp -v ./config/usr_local_bin/* /usr/local/bin/
 
