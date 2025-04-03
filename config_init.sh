672306b61059228848687941593a355d69038ae1
Bobby Valenzuela <70870649+bobby-valenzuela@users.noreply.github.com>
2025-04-03 11:12:34 -0700

    Update config_init.sh

--- config_init.sh
+++ config_init.sh
@@ -36,10 +36,7 @@ cp -v ./config/usr_local_bin/* /usr/local/bin/
 
 if [[ "$1" == "install" ]];then
     printf "[+] Installing packages...\n\n"
-    sudo apt-get update && apt install python3 wget zsh lua5.4 curl tar ripgrep fzf make gcc unzip git git-all xclip build-essential p7zip-full jq locatesshpass xsel cmake libstdc++6 -y
-
-    printf "[+] Installing oh-my-zsh...\n\n"
-    sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+    sudo apt-get update && apt install python3 wget zsh lua5.4 curl tar ripgrep fzf make gcc unzip git git-all xclip build-essential p7zip-full jq locate sshpass xsel cmake libstdc++6 -y
 
     printf "[+] Installing latest neovim stable release...\n\n"
     curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
@@ -47,11 +44,14 @@ if [[ "$1" == "install" ]];then
     sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
     echo 'export PATH="/opt/nvim-linux-x86_64/bin:$PATH"' >> $HOME/.zshrc
 
-    printf "[+] Installing powerlevel10k...\n\n"
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-
     printf "[+] Installing nvm...\n\n"
     wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+
+    printf "[+] Installing oh-my-zsh...\n\n"
+    sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+    printf "[+] Installing powerlevel10k...\n\n"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
     printf "[+] Sourcing .zshrc ...\n\n"
     source $HOME/.zshrc
