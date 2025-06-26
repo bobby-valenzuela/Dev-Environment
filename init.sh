90137cb39315f39ba9c06636d06c9ef60bc161bc
Bobby Valenzuela <70870649+bobby-valenzuela@users.noreply.github.com>
2025-06-26 08:42:41 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,1 @@
-git clone https://github.com/bobby-valenzuela/Dev-Environment.git && cd Dev-Environment && sh ./install.sh install
+git clone https://github.com/bobby-valenzuela/Dev-Environment.git && cd Dev-Environment && sh ./install.sh config
