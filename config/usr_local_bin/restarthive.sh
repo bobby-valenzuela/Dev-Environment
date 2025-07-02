22e6da11a9f0e1048dbabb3d0e1e6bf797302132
Bobby Valenzuela <bobby.valenzuela@protonmail.com>
2025-07-02 10:25:15 -0700

    updates to shell scripts

--- config/usr_local_bin/restarthive.sh
+++ config/usr_local_bin/restarthive.sh
@@ -3,5 +3,7 @@
 ssh hive01 'sudo service apache2 restart && echo restarted_1'
 ssh hive02 'sudo service apache2 restart && echo restarted_2'
 ssh hive03 'sudo service apache2 restart && echo restarted_3'
+ssh hive04 'sudo service apache2 restart && echo restarted_4'
+ssh hive05 'sudo service apache2 restart && echo restarted_5'
 # ssh beta02 'sudo service apache2 restart'
 
