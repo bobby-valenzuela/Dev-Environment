8e91b047f1f5ae44935dbf6c4b4d1883cef39229
Bobby Valenzuela <bobby.valenzuela@protonmail.com>
2025-07-02 23:34:45 -0700

    added more mini plugins

--- config/.config/nvim/lua/plugins/mini.lua
+++ config/.config/nvim/lua/plugins/mini.lua
@@ -1,4 +1,5 @@
 return {
+    { 'echasnovski/mini.diff', version = '*' },
     {
         "echasnovski/mini.nvim",
         version = false,
@@ -9,6 +10,17 @@ return {
                     comment = "gc",
                     comment_line = "gcc",
                     textobject = "gc",
+                    -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+                    left = '<C-Left>',
+                    right = '<C-Right>',
+                    down = '<C-Down>',
+                    up = '<C-Up>',
+
+                    -- Move current line in Normal mode
+                    line_left = '<C-Left>',
+                    line_right = '<C-Right>',
+                    line_down = '<C-Down>',
+                    line_up = '<C-Up>',
                 },
                   options = {
                     -- Automatically reindent selection during linewise vertical move
